0f077f430ed04c5c9fb3ae6163c400f140e4fb3c
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-01-31 15:37:31 +0530

    Added Task2.2

---
+++ Module1/Task2.2.ino
@@ -0,0 +1,29 @@
+
+int pir=2;
+int buzzer=12;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pir,INPUT);
+   pinMode(buzzer,OUTPUT);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(pir),Act,CHANGE);
+}
+
+void loop() 
+{
+  delay(500);
+}
+void Act() 
+{
+  if(digitalRead(pir)==HIGH){
+    Serial.println("Motion Detected");
+    digitalWrite(13,LOW);
+  digitalWrite(buzzer,HIGH);
+  }
+  else{
+    digitalWrite(buzzer,LOW);
+        digitalWrite(13,HIGH);
+  }
+}
